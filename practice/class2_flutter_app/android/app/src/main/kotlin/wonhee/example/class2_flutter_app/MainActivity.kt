66c3c8860d3d02abb3f3c02381080ba3d570e988
KIM WONHEE <soyoung0007@naver.com>
2021-02-12 17:22:01 +0900

    .

---
+++ practice/class2_flutter_app/android/app/src/main/kotlin/wonhee/example/class2_flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package wonhee.example.class2_flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ practice/class2_flutter_app/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "First App",
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple
+      ),
+
+      home: MyHomePage(),
+
+    );
+
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title : Text("앱 바 위치"),
+      ),
+      body: Center(
+        child: Column(
+          children:<Widget> [
+            Text("hello"),
+            Text("hello"),
+            Text("hello"),
+
+
+          ],
+        ),
+      ),
+
+    );
+  }
+}
