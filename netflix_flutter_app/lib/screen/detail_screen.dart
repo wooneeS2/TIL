add8e12dc20af3f87e0b07229577677ba00d3650
woonees2 <hello_world_@kakao.com>
2021-03-07 00:25:27 +0900

    .
    수정필요

--- netflix_flutter_app/lib/screen/detail_screen.dart
+++ netflix_flutter_app/lib/screen/detail_screen.dart
@@ -33,17 +33,143 @@ class _DetailScreenState extends State<DetailScreen> {
                   decoration: BoxDecoration(image: DecorationImage(image: AssetImage('images/'+widget.movie.poster),
                     fit:BoxFit.cover,
                   ),
+                  ),
+                    child: ClipRect(child: BackdropFilter(filter: ImageFilter.blur(sigmaX: 10, sigmaY:10 ),
+                    child: Container(
+                      alignment: Alignment.center,
+                      color: Colors.black.withOpacity(0.1),
+                      child: Container(child: Column(
+                        children: <Widget>[
+                          Container(
+                            padding:EdgeInsets.fromLTRB(0, 45, 0, 10),
+                            child: Image.asset('images/'+widget.movie.poster),
+                            height: 300,
+                          ),
+                          Container(
+                            padding:EdgeInsets.all(7) ,
+                            child:Text("99% 일치", textAlign: TextAlign.center, style:TextStyle(fontSize: 13),)
+                          ),
+
+                          Container(
+                              padding:EdgeInsets.all(7) ,
+                              child:Text(widget.movie.title, textAlign: TextAlign.center, style:TextStyle(fontWeight:FontWeight.bold, fontSize: 16),)
+                          ),
+                          Container(
+                            padding:  EdgeInsets.all(3),
+                            child: TextButton(
+                              onPressed: () {},
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+
+                                children: <Widget>[
+                                  Icon(Icons.play_arrow),
+                                  Text("재생"),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(5),
+                            child:Text(widget.movie.toString(),),
+                          ),
+
+                          Container(
+                            padding: EdgeInsets.all(5),
+                            alignment: Alignment.center,
+                            child:Text("출연 : 어쩌구들",
+                            style: TextStyle(color:Colors.white60,fontSize: 12),),
+                          ),
+                          Positioned(
+                            child: AppBar(
+                              backgroundColor: Colors.transparent,
+                              elevation: 0,
+                            ),
+                          ),
+
+                        ],
+
+
+                      ),),
+                    ),),
+
+                    ),
+
+
 
                   ),
 
-
-                  )
-
                 ],
-              )
+              ),
+                    Container(
+                   color: Colors.black26,
+                    child: Row(
+                     mainAxisAlignment: MainAxisAlignment.start,
+                   children: <Widget>[
+         Container(
+            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+             child: InkWell(
+              onTap: () {},
+             child: Column(
+               children: <Widget>[
+                   like ? Icon(Icons.check) : Icon(Icons.add),
+                      Padding(
+                      padding: EdgeInsets.all(5),
+                      ),
+                 Text(
+                   '내가 찜한 콘텐츠',
+                   style: TextStyle(
+                     fontSize: 11,
+                     color: Colors.white60,
+                   ),
+                 )
+               ],
+             ),
+             ),
+         ),
+                     Container(
+                       padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                       child: Container(
+                         child: Column(
+                           children: <Widget>[
+                             Icon(Icons.thumb_up),
+                             Padding(
+                               padding: EdgeInsets.all(5),
+                             ),
+                             Text(
+                               '평가',
+                               style: TextStyle(
+                                   fontSize: 11, color: Colors.white60),
+                             )
+                           ],
+                         ),
+                       ),
+                     ),
+
+                     Container(
+                       padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                       child: Container(
+                         child: Column(
+                           children: <Widget>[
+                             Icon(Icons.send),
+                             Padding(padding: EdgeInsets.all(5)),
+                             Text(
+                               '공유',
+                               style: TextStyle(
+                                   fontSize: 11, color: Colors.white60),
+                             ),
+                           ],
+                         ),
+                       ),
+                     )
+
+
+
             ],
           ),
         ),
+        ],
+      ),
+    )
       ),
     );
   }
