f844d0776fc54e680ff3104dfebfb8af5994d0b7
woonees2 <hello_world_@kakao.com>
2021-06-02 00:39:04 +0900

    practice call back
    출처 : 유튜브 코딩파파( https://youtu.be/cuT1koj18yU )

--- practice/practice_flutter_app/lib/main.dart
+++ practice/practice_flutter_app/lib/main.dart
@@ -1,83 +1,89 @@
 import 'package:flutter/material.dart';
-import 'login_page.dart';
 
-void main(){
-  runApp(new MyApp());
+//flutter 함수 전달(call back) 연습
+//출처 - 유튜브 코딩파파 (https://youtu.be/cuT1koj18yU)
 
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title:'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-      ),
-      home:MyHomePage(title:'Flutter Demo Home Page'),
-    );
-  }
-}
+  const MyApp({Key key}) : super(key: key);
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState()=> _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage>{
-  int _counter = 0;
-  void _incrementCounter(){
-    setState(() {
-      _counter++;
-    });
-  }
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar:AppBar(
-        title:Text(widget.title),
-      ),
-      body:Center(
-        child:Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'counts: ',
-            ),
-            Text(
-              '$_counter',
-              style:Theme.of(context).textTheme.display1,
-            ),
-          ],
-        )
-
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        onTap:(index) => {},
-        currentIndex: 0,
-        items: [
-          new BottomNavigationBarItem(
-            icon:Icon(Icons.mail),
-            title:Text('First'),
-          ),
-          new BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            title:Text('Second')
-          )
-        ]
-      ),
+    return MaterialApp(
+      title: 'flutter Demo',
+      home: PracticepPassData(),
     );
   }
 }
 
+class PracticepPassData extends StatefulWidget {
+  PracticepPassData({Key key}) : super(key: key);
 
+  @override
+  _PracticepPassDataState createState() => _PracticepPassDataState();
+}
+
+class _PracticepPassDataState extends State<PracticepPassData> {
+  int dataToSend = 0;
+  int returnData = -1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("numbering"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            child: Text('returnData - $returnData'),
+          ),
+          Container(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  dataToSend++;
+                });
+              },
+              child: Text('dataToSend - $dataToSend'),
+            ),
+          ),
+          OtherWidget(
+            received: dataToSend,
+            function: returnDataFunction,
+          ),
+        ],
+      ),
+    );
+  }
+
+  void returnDataFunction(int returnDt) {
+    setState(() {
+      returnData = returnDt;
+    });
+  }
+}
+
+class OtherWidget extends StatelessWidget {
+  const OtherWidget({
+    Key key,
+    @required this.received,
+    this.function,
+  }) : super(key: key);
+
+  final int received;
+  final Function function;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TextButton(
+          onPressed: () {
+            function(received * 2);
+          },
+          child: Text('received Data - $received')),
+    );
+  }
+}
