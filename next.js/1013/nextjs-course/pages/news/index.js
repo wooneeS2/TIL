daf627b9868838d7f0a14b067e3bbf542dfe0f52
woonees2 <hello_world_@kakao.com>
2022-10-13 21:33:16 +0900

    next link 사용법

--- next.js/1013/nextjs-course/pages/news/index.js
+++ next.js/1013/nextjs-course/pages/news/index.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Link from "next/link";
 
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/one-link">첫번째 링크</Link>
+        </li>
+        <li>
+          <Link href="/news/two-link">두번째 링크</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default NewsPage;
