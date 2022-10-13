fefb5c9fb1425b154f93416a67b4bc32d82f6bbe
woonees2 <hello_world_@kakao.com>
2022-10-13 21:24:04 +0900

    next route 사용법

---
+++ next.js/1013/nextjs-course/pages/news/[newsId].js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+function DetailPage() {
+  const router = useRouter();
+  const newsId = router.query.newsId;
+  return <h1>The {newsId} Page</h1>;
+}
+
+export default DetailPage;
