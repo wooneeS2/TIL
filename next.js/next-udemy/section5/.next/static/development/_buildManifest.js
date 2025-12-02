ba001b2ae846624d042b92ca1d2058d76a0c3418
woonees2 <soyoung0007@naver.com>
2025-12-02 09:30:21 -0500

    Organaize folder

--- next.js/next-udemy/section5/.next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/last-sales":["static\u002Fchunks\u002Fpages\u002Flast-sales.js"],sortedPages:["\u002F_app","\u002F_error","\u002Flast-sales"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
